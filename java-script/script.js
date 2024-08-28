e3cbabae989797aae8b1741bdaad9c9780599dac
Maruf Khan <marufatrai12@gmail.com>
2024-08-28 19:38:40 +0600

    JS For Loop

---
+++ java-script/script.js
@@ -0,0 +1,23 @@
+//     var m = parseInt(prompt("Enter The String Number : "));
+//     var n = parseInt(prompt("Enter The Last Number : "));
+
+//     var sum = 0;
+
+//    for(var x = m; x <= n; x++){
+
+//     sum = sum + x;
+//    }
+
+//    document.write(sum);
+
+
+    for(var x = 1; x <= 5; x++){
+
+    var num1 = parseInt(prompt("Enter a First Number : "));
+    var num2 = parseInt(prompt("Enter a Second Number : "));
+
+    var result = num1 + num2;
+
+    console.log(num1 + " + " + num2 + " = " + result);
+
+    }
